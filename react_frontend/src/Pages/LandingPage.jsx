1c467d4781068fbb553770524a154123c4f33b71
eliyas41 <fentaeliyas@gmail.com>
2025-03-02 22:55:27 +0300

    Landing page

---
+++ react_frontend/src/Pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import { Hero } from "../components/Hero/Hero";
+
+class Landing extends Component {
+    render() {
+        return (
+            <section>
+                <Hero />
+            </section>
+        );
+    }
+}
+
+export default Landing;
---
+++ react_frontend/src/api.config.js
@@ -0,0 +1,2 @@
+// use axios to make api requests
+export const API_URL = "http://127.0.0.1:2030/api/v1";
