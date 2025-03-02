ada23181cf018e5a086abef58e27a35a2690aaad
eliyas41 <fentaeliyas@gmail.com>
2025-03-02 17:32:10 +0300

    Create signup endpoint with hashed password storage

--- server/app/app.js
+++ server/app/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import dbConnect from '../config/dbConnect.js';
+import userRouter from '../routes/userRoute.js';
 
 // db connect
 dbConnect();
@@ -12,5 +13,8 @@ app.use(express.json());
 
 //url encoded
 app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/api/v1/users/", userRouter);
 
 export default app;
---
+++ server/controllers/userCtrl.js
@@ -0,0 +1,35 @@
+import User from "../model/User.js";
+import bcrypt from 'bcryptjs'
+
+// @desc    Register user
+// @route   POST /api/v1/users/signup
+// @access  Private/Admin
+export const registerUserCtrl = async (req, res) => {
+  const { fullname, email, password } = req.body;
+
+  // Check user exist
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    // throw
+    return res.status(400).json({
+      status: "fail",
+      message: "User already exist",
+    });
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  // Create the user
+  const user = await User.create({
+    fullname,
+    email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({
+    status: "success",
+    message: "User Registered Successfully",
+    data: user,
+  });
+};
