ebad9d798f728e65d2ecc914ba9abc1470e33377
eliyas41 <fentaeliyas@gmail.com>
2025-03-02 23:03:10 +0300

    Login and Signup page

--- react_frontend/src/Router.jsx
+++ react_frontend/src/Router.jsx
@@ -1,9 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
+// import UsersPage from "./Pages/UsersPage";
+// import UserDetailsPage from "./Pages/UserDetailsPage";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/signUp";
+
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <LandingPage />,
     },
+    {
+        path: "/signup",
+        element: <SignUp />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    // {
+    //     path: "/users",
+    //     element: <UsersPage />,
+    // },
+    // {
+    //     path: "/users/:id",
+    //     element: <UserDetailsPage />,
+    // },
 ]);
