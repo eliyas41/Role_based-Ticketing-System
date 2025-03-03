47b4262329c6996a4411d289a6a6fdac2a13392f
eliyas41 <fentaeliyas@gmail.com>
2025-03-03 16:38:05 +0300

    Human icon added for the admin UI

--- react_frontend/src/components/Hero/Hero.jsx
+++ react_frontend/src/components/Hero/Hero.jsx
@@ -38,6 +38,28 @@ export class Hero extends Component {
                         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
                             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
                                 <span className="relative inline-block">
+                                    <svg
+                                        viewBox="0 0 52 24"
+                                        fill="currentColor"
+                                        className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                                    >
+                                        <defs>
+                                            <pattern
+                                                id="e77df901-b9d7-4b9b-822e-16b2d410795b"
+                                                x="0"
+                                                y="0"
+                                                width=".135"
+                                                height=".30"
+                                            >
+                                                <circle cx="1" cy="1" r=".7" />
+                                            </pattern>
+                                        </defs>
+                                        <rect
+                                            fill="url(#e77df901-b9d7-4b9b-822e-16b2d410795b)"
+                                            width="52"
+                                            height="24"
+                                        />
+                                    </svg>
                                     <span className="relative text-red-500">Demo</span>
                                 </span>{" "}
                                 App, built with NodeJs and ReactJs
@@ -46,10 +68,12 @@ export class Hero extends Component {
                                 Role-Based Ticketing System
                             </p>
                             <p className="text-base text-gray-700 md:text-lg">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-                                praesentium quaerat fugit! Excepturi nostrum voluptates voluptate
-                                laborum pariatur, maiores illo doloremque, similique eum
-                                exercitationem aliquam suscipit totam sit magni soluta!
+                                Lorem ipsum dolor sit amet consectetur adipisicing
+                                elit. Aliquid praesentium quaerat fugit! Excepturi
+                                nostrum voluptates voluptate laborum pariatur,
+                                maiores illo doloremque, similique eum
+                                exercitationem aliquam suscipit totam sit magni
+                                soluta!
                             </p>
                         </div>
 
